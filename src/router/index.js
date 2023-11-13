4391b87d1c97334d73d68c9ca5196bf7e81dbe77
YLLA1215 <325936216@qq.com>
2023-11-13 12:18:18 +0800

    新增注册和登录页面

--- src/router/index.js
+++ src/router/index.js
@@ -4,9 +4,12 @@ import {
   createWebHashHistory,
 } from "vue-router";
 
-import Index from "../views/index/index.vue";
-import Home from "../views/home/index.vue";
-import Record from "../views/record/index.vue";
+import Index from "@/views/index/index.vue";
+import Home from "@/views/home/index.vue";
+import Record from "@/views/record/index.vue";
+import User from "@/views/user/index.vue";
+import Login from "@/views/login/index.vue";
+import Register from "@/views/register/index.vue";
 
 const routes = [
   {
@@ -29,9 +32,27 @@ const routes = [
         meta:{
           index: 1
         }
+      },
+      {
+        path: "/user",
+        name: "User",
+        component: User,
+        meta:{
+          index: 2
+        }
       }
     ],
   },
+  {
+    path: "/login",
+    name: "Login",
+    component: Login,
+  },
+  {
+    path: "/register",
+    name:"Register",
+    component: Register
+  }
 ];
 
 const router = createRouter({
