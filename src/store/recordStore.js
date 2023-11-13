95d60033de9a93d9d1bc10b4e310601520f9fa3e
YLLA1215 <325936216@qq.com>
2023-11-13 12:19:07 +0800

    新增必要的全局状态

--- src/store/recordStore.js
+++ src/store/recordStore.js
@@ -3,13 +3,14 @@ import { getAverageOf5, getAverageOf12 } from "@/utils/record";
 import {
   formatTimestampByformatedDurationInGame,
   formatTimestampByformatedDurationInRecord,
-  formatDurationInRecord
+  formatDurationInRecord,
 } from "@/utils/time";
 export const useRecordStore = defineStore("record", {
   persist: true,
 
   state: () => ({
     record: [],
+    unUploadRecord: [],
   }),
   getters: {
     getRecord() {
@@ -17,7 +18,23 @@ export const useRecordStore = defineStore("record", {
         const index = itemIndex + 1;
         const ao5 = getAverageOf5(this.record.slice(0, index));
         const ao12 = getAverageOf12(this.record.slice(0, index));
-        const duration = formatDurationInRecord(item.duration)
+        const duration = formatDurationInRecord(item.duration);
+
+        return {
+          ...item,
+          index,
+          duration,
+          ao5,
+          ao12,
+        };
+      });
+    },
+    getUnUploadRecord() {
+      return this.unUploadRecord.map((item, itemIndex) => {
+        const index = itemIndex + 1;
+        const ao5 = getAverageOf5(this.unUploadRecord.slice(0, index));
+        const ao12 = getAverageOf12(this.unUploadRecord.slice(0, index));
+        const duration = formatDurationInRecord(item.duration);
 
         return {
           ...item,
@@ -30,6 +47,9 @@ export const useRecordStore = defineStore("record", {
     }
   },
   actions: {
+    setRecord(record) {
+      this.record = record;
+    },
     addRecord(record) {
       this.record.push(record);
     },
@@ -44,6 +64,29 @@ export const useRecordStore = defineStore("record", {
       for (const key in record) {
         this.record[index][key] = record[key];
       }
-    }
+    },
+    setUnUploadRecord(record) {
+      this.unUploadRecord = record;
+    },
+    addUnUploadRecord(record) {
+      this.unUploadRecord.push(record);
+    },
+    clearUnUploadRecord() {
+      this.unUploadRecord = [];
+    },
+    deleteUnUploadRecordByDateTime(dateTime) {
+      this.unUploadRecord = this.unUploadRecord.filter(
+        (item) => item.dateTime !== dateTime
+      );
+    },
+
+    updateUnUploadRecord(record) {
+      const index = this.unUploadRecord.findIndex(
+        (item) => item.id === record.id
+      );
+      for (const key in record) {
+        this.unUploadRecord[index][key] = record[key];
+      }
+    },
   },
 });
--- src/store/userStore.js
+++ src/store/userStore.js
@@ -1,9 +1,45 @@
 import { defineStore } from "pinia";
+import {
+  formatTimestampByformatedDurationInGame,
+  formatTimestampByformatedDurationInRecord,
+  formatDurationInRecord,
+} from "@/utils/time";
 
 export const useUserStore = defineStore("user", {
   persist: true,
 
-  state: () => ({}),
-  getters: {},
-  actions: {},
+  state: () => ({
+    user: null,
+    userBestRecord: null,
+    token: "",
+  }),
+  getters: {
+    getBestSingleRecord() {
+      if (this.userBestRecord) {
+        return formatDurationInRecord(
+          this.userBestRecord.bestSingle
+        );
+      }
+      return null;
+    },
+    getBestAverageRecord() {
+      if (this.userBestRecord) {
+        return formatDurationInRecord(
+          this.userBestRecord.bestAverage
+        );
+      }
+      return null;
+    }
+  },
+  actions: {
+    setUser(user) {
+      this.user = user;
+    },
+    setUserBestRecord(userBestRecord) {
+      this.userBestRecord = userBestRecord;
+    },
+    setToken(token) {
+      this.token = token;
+    }
+  },
 });
