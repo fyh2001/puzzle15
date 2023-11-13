5bba53f2814065349d28d8fa765086be94c5edc2
YLLA1215 <325936216@qq.com>
2023-11-13 12:18:27 +0800

    新增请求拦截器

--- src/api/index.js
+++ src/api/index.js
@@ -1,9 +1,12 @@
 import axios from "axios";
+import { useUserStore } from "@/store/userStore";
 
-const baseURL = "http://localhost:8080";
+// const baseURL = "http://localhost:8080/api";
+// const baseURL = "http://192.168.31.141:8080/api";
+const baseURL = "http://139.9.7.92:8081/api";
 
-const instance = axios.create({
-  baseURL: baseURL,
+const request = axios.create({
+  baseURL,
   timeout: 1000,
   headers: {
     "Content-Type": "application/json",
@@ -13,13 +16,31 @@ const instance = axios.create({
 /**
  * 请求拦截器
  */
-instance.interceptors.request.use();
+request.interceptors.request.use(
+  (config) => {
+    const userStore = useUserStore();
+    const token = userStore.token;
+    if (token || token !== "") {
+      config.headers.Authorization = token;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 /**
  * 响应拦截器
  */
-instance.interceptors.response.use();
+request.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 // 暴露所写的内容
-export { instance as request };
-export { baseURL };
+export { request, baseURL };
